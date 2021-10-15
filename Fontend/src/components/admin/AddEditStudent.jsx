91e447608225ab527a51994012bba71a272e4d80
NamTran <nam.tran@zamo.io>
2021-10-15 20:59:58 +0700

    a

--- Fontend/src/components/admin/AddEditStudent.jsx
+++ Fontend/src/components/admin/AddEditStudent.jsx
@@ -1,51 +1,43 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import {addMember, updateMember} from "../../service/MemberService";
 import {showNotification} from "../common/NotifyCation";
 import ImageUpload from "../common/ImageUpload";
-import {handleInput} from "../common/Utils";
+import {storage} from "../common/firebase/Config";
 
-class AddEditStudent extends Component {
-    constructor(props) {
-        super(props);
+function AddEditStudent(props) {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [file, setFile] = useState(null);
+    const [url, setUrl] = useState("");
 
-        this.state = {
-            name: "",
-            email: "",
-            password: "",
-        };
-
-        this.child = React.createRef();
-        this.onSubmit = this.onSubmit.bind(this);
-        this.handleInput = handleInput.bind(this);
-    }
-
-    onSubmit(e) {
-        e.preventDefault();
-        if (this.props.student._id === -1) {
+    function onSubmit(e) {
+        // e.preventDefault();
+        if (props.student._id === -1) {
             addMember(
-                this.state.name,
-                this.state.email,
-                this.state.password
+                name,
+                email,
+                password
             ).then((Response) => {
                 if (Response.data.code !== -9999) {
-                    this.child.current.handleUpload(
+                    handleUpload(
                         "avatar-" + Response.data.payload._id
                     );
                     showNotification("success_add");
-                    this.props.reload();
-                    this.props.close_modal();
+                    props.reload();
+                    props.close_modal();
                 } else {
                     showNotification(Response.data.message);
                 }
             });
         } else {
-            updateMember(this.props.student._id, this.state.name).then(
+            updateMember(props.student._id, name).then(
                 (Response) => {
                     if (Response.data.code !== -9999) {
-                        this.child.current.handleUpload(this.props.student._id);
+                        handleUpload(props.student._id);
                         showNotification("success_update");
-                        this.props.close_modal();
-                        this.props.reload();
+                        props.close_modal();
+                        props.reload();
                     } else {
                         showNotification(Response.data.message);
                     }
@@ -54,20 +46,29 @@ class AddEditStudent extends Component {
         }
     }
 
-    render() {
+    function handleUpload(name) {
+        const { image } = {url: url, file: file};
+        storage.ref(`images/${name}.png`).put(image);
+    }
+
+    function onSetImage(url, file) {
+        setUrl(url);
+        setFile(file);
+    }
+
         return (
-            <div hidden={!this.props.show_add} className="custom-css-001">
+            <div hidden={!props.show_add} className="custom-css-001">
                 <div
                     className="custom-css-002"
-                    onClick={() => this.props.close_modal()}
-                ></div>
+                    onClick={() => props.close_modal()}
+                />
                 <div className="modal-content custom-css-003">
                     <form className="needs-validation" noValidate>
                         <div className="modal-header">
                             <h4>Thông tin học viên</h4>
                             <button
                                 className="btn btn-link"
-                                onClick={() => this.props.close_modal()}
+                                onClick={() => props.close_modal()}
                             >
                                 <i className="fas fa-times"></i>
                             </button>
@@ -75,7 +76,7 @@ class AddEditStudent extends Component {
                         <div className="modal-body custom-css-004">
                             <div className="form-group"></div>
                             <div className="form-group">
-                                <ImageUpload ref={this.child} url={this.props.student.avatar} />
+                                <ImageUpload url={url} onSetImage={onSetImage}/>
                             </div>
                             <div className="form-group">
                                 <label>Họ và tên</label>
@@ -83,12 +84,14 @@ class AddEditStudent extends Component {
                                     id="name"
                                     type="text"
                                     className="form-control"
-                                    onChange={this.handleInput}
+                                    onChange={(event)=>{
+                                        setName(event.target.value)
+                                    }}
                                     required
                                     value={
-                                        this.state.name === ""
-                                            ? this.props.student.name
-                                            : this.state.name
+                                        name === ""
+                                            ? props.student.name
+                                            : name
                                     }
                                 />
                                 <div className="invalid-feedback">What's your name?</div>
@@ -99,34 +102,38 @@ class AddEditStudent extends Component {
                                     id="email"
                                     type="email"
                                     className="form-control"
-                                    onChange={this.handleInput}
+                                    onChange={(event)=>{
+                                        setEmail(event.target.value)
+                                    }}
                                     required
                                     value={
-                                        this.state.email === ""
-                                            ? this.props.student.email
-                                            : this.state.email
+                                        email === ""
+                                            ? props.student.email
+                                            : email
                                     }
                                 />
                                 <div className="invalid-feedback">Oh no! Email is invalid.</div>
                             </div>
                             <div
                                 className="form-group"
-                                hidden={this.props.student._id !== -1 ? true : false}
+                                hidden={props.student._id !== -1}
                             >
                                 <label>Mật khẩu</label>
                                 <input
                                     id="password"
                                     type="password"
                                     className="form-control"
-                                    onChange={this.handleInput}
+                                    onChange={(event)=>{
+                                        setPassword(event.target.value)
+                                    }}
                                     required
-                                    value={this.state.password}
+                                    value={password}
                                 />
                                 <div className="valid-feedback">Good job!</div>
                             </div>
                         </div>
                         <div className="modal-footer text-right">
-                            <button className="btn btn-primary" onClick={this.onSubmit}>
+                            <button className="btn btn-primary" onClick={onSubmit}>
                                 Submit
                             </button>
                         </div>
@@ -134,7 +141,6 @@ class AddEditStudent extends Component {
                 </div>
             </div>
         );
-    }
 }
 
 AddEditStudent.defaultProps = {
